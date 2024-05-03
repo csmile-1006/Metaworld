8b5a52598101aa9290e0341c057f99b1cca9a069
csmile-1006 <doli1573@kaist.ac.kr>
2024-05-03 17:27:00 +0900

    MISC changes for stage indicator

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_faucet_close_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_faucet_close_v2.py
@@ -8,7 +8,6 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _asser
 
 class SawyerFaucetCloseEnvV2(SawyerXYZEnv):
     def __init__(self):
-
         hand_low = (-0.5, 0.40, -0.15)
         hand_high = (0.5, 1, 0.5)
         obj_low = (-0.1, 0.8, 0.0)
@@ -22,12 +21,9 @@ class SawyerFaucetCloseEnvV2(SawyerXYZEnv):
             hand_high=hand_high,
         )
 
-        self.init_config = {
-            'obj_init_pos': np.array([0, 0.8, 0.0]),
-            'hand_init_pos': np.array([0., .4, .2])
-        }
-        self.hand_init_pos = self.init_config['hand_init_pos']
-        self.obj_init_pos = self.init_config['obj_init_pos']
+        self.init_config = {"obj_init_pos": np.array([0, 0.8, 0.0]), "hand_init_pos": np.array([0.0, 0.4, 0.2])}
+        self.hand_init_pos = self.init_config["hand_init_pos"]
+        self.obj_init_pos = self.init_config["obj_init_pos"]
 
         goal_low = self.hand_low
         goal_high = self.hand_high
@@ -40,49 +36,44 @@ class SawyerFaucetCloseEnvV2(SawyerXYZEnv):
 
     @property
     def model_name(self):
-        return full_v2_path_for('sawyer_xyz/sawyer_faucet.xml')
+        return full_v2_path_for("sawyer_xyz/sawyer_faucet.xml")
 
     @_assert_task_is_set
     def evaluate_state(self, obs, action):
-        (reward, tcp_to_obj, _, target_to_obj, object_grasped,
-         in_place) = self.compute_reward(action, obs)
+        (reward, tcp_to_obj, _, target_to_obj, object_grasped, in_place) = self.compute_reward(action, obs)
 
         info = {
-            'success': float(target_to_obj <= 0.07),
-            'near_object': float(tcp_to_obj <= 0.01),
-            'grasp_success': 1.,
-            'grasp_reward': object_grasped,
-            'in_place_reward': in_place,
-            'obj_to_target': target_to_obj,
-            'unscaled_reward': reward,
+            "success": float(target_to_obj <= 0.07),
+            "near_object": float(tcp_to_obj <= 0.01),
+            "grasp_success": 1.0,
+            "grasp_reward": object_grasped,
+            "in_place_reward": in_place,
+            "obj_to_target": target_to_obj,
+            "unscaled_reward": reward,
         }
+        info["skill"] = float(info["near_object"]) + float(info["success"])
 
         return reward, info
 
     @property
     def _target_site_config(self):
-        return [('goal_close', self._target_pos),
-                ('goal_open', np.array([10., 10., 10.]))]
+        return [("goal_close", self._target_pos), ("goal_open", np.array([10.0, 10.0, 10.0]))]
 
     def _get_quat_objects(self):
-        return self.sim.data.get_body_xquat('faucetBase')
+        return self.sim.data.get_body_xquat("faucetBase")
 
     def _get_pos_objects(self):
-        return self._get_site_pos('handleStartClose') + np.array(
-            [0., 0., -0.01])
+        return self._get_site_pos("handleStartClose") + np.array([0.0, 0.0, -0.01])
 
     def reset_model(self):
         self._reset_hand()
 
         # Compute faucet position
-        self.obj_init_pos = self._get_state_rand_vec() if self.random_init \
-            else self.init_config['obj_init_pos']
+        self.obj_init_pos = self._get_state_rand_vec() if self.random_init else self.init_config["obj_init_pos"]
         # Set mujoco body to computed position
-        self.sim.model.body_pos[self.model.body_name2id(
-            'faucetBase')] = self.obj_init_pos
+        self.sim.model.body_pos[self.model.body_name2id("faucetBase")] = self.obj_init_pos
 
-        self._target_pos = self.obj_init_pos + np.array(
-            [-self._handle_length, .0, .125])
+        self._target_pos = self.obj_init_pos + np.array([-self._handle_length, 0.0, 0.125])
 
         return self._get_obs()
 
@@ -95,16 +86,16 @@ class SawyerFaucetCloseEnvV2(SawyerXYZEnv):
         tcp = self.tcp_center
         target = self._target_pos.copy()
 
-        target_to_obj = (obj - target)
+        target_to_obj = obj - target
         target_to_obj = np.linalg.norm(target_to_obj)
-        target_to_obj_init = (self.obj_init_pos - target)
+        target_to_obj_init = self.obj_init_pos - target
         target_to_obj_init = np.linalg.norm(target_to_obj_init)
 
         in_place = reward_utils.tolerance(
             target_to_obj,
             bounds=(0, self._target_radius),
             margin=abs(target_to_obj_init - self._target_radius),
-            sigmoid='long_tail',
+            sigmoid="long_tail",
         )
 
         faucet_reach_radius = 0.01
@@ -114,7 +105,7 @@ class SawyerFaucetCloseEnvV2(SawyerXYZEnv):
             tcp_to_obj,
             bounds=(0, faucet_reach_radius),
             margin=abs(tcp_to_obj_init - faucet_reach_radius),
-            sigmoid='gaussian',
+            sigmoid="gaussian",
         )
 
         tcp_opened = 0
@@ -124,5 +115,4 @@ class SawyerFaucetCloseEnvV2(SawyerXYZEnv):
         reward *= 2
         reward = 10 if target_to_obj <= self._target_radius else reward
 
-        return (reward, tcp_to_obj, tcp_opened, target_to_obj, object_grasped,
-                in_place)
+        return (reward, tcp_to_obj, tcp_opened, target_to_obj, object_grasped, in_place)
--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_push_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_push_v2.py
@@ -82,7 +82,7 @@ class SawyerPushEnvV2(SawyerXYZEnv):
             "obj_to_target": target_to_obj,
             "unscaled_reward": reward,
         }
-        info["skill"] = float(info["grasp_success"]) + float(info["sucesss"])
+        info["skill"] = float(info["grasp_success"]) + float(info["success"])
 
         return reward, info
 
